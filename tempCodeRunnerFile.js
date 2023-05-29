9350dc944f8e90b4f429eb77a25ad82871720c4f
qStentyq <stenty2s@gmail.com>
2023-05-29 20:27:59 +0300

    Lab6TSI

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let recomendTemplate = `
+	<h2 class="title">Ваши рекомендации</h2>
+	<h3 class="summary">%message%</h3>
+	<p class="result">%resultGeneral%</p>
+	<p class="result">%resultInformation%</p>
+	<p class="result">%resultHuman%</p>
+`;
+	recomendTemplate += `\t<p>111</p>`
+	console.log(recomendTemplate)
